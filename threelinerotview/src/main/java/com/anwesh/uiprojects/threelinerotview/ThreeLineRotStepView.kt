e211ba28a3c21887e19c26f5d4dae45dbc1feb3f
Anwesh43 <anweshthecool0@gmail.com>
2018-11-18 16:00:52 +0530

    adding drawTLRNode canvas extension method

--- threelinerotview/src/main/java/com/anwesh/uiprojects/threelinerotview/ThreeLineRotStepView.kt
+++ threelinerotview/src/main/java/com/anwesh/uiprojects/threelinerotview/ThreeLineRotStepView.kt
@@ -31,4 +31,33 @@ fun Float.getMirrorValue(a : Int, b : Int) : Float = (1 - this) * a.getInverse()
 
 fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = dir * scGap * getScaleFactor().getMirrorValue(a, b)
 
-
+fun Canvas.drawTLRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes)
+    val size : Float = gap / SIZE_FACTOR
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val sc11 : Float = sc1.divideScale(0, 2)
+    val sc12 : Float = sc2.divideScale(1, 2)
+    paint.color = COLOR
+    paint.strokeWidth = Math.min(w, h) / STROKE_FACTOR
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(gap * (i + 1), h/2)
+    rotate(90f * sc2)
+    drawLine(-size, -size, size, -size, paint)
+    var y : Float = -size
+    for (j in 0..(lines - 1)) {
+        val sca : Float = sc11.divideScale(j, lines)
+        val scl : Float = sc12.divideScale(j, lines)
+        y += size * sca
+        drawLine(-size, y, size, y, paint)
+        for (k in 0..1) {
+            val x : Float = -size + k * 2 * size
+            val y : Float = -size + j * size
+            drawLine(x, y, x, y + size * scl, paint)
+        }
+    }
+    restore()
+}
