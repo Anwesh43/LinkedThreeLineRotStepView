19f7410b689063129f8ef08c1fece5775057e501
Anwesh43 <anweshthecool0@gmail.com>
2018-11-18 16:20:00 +0530

    adding Animator class

--- threelinerotview/src/main/java/com/anwesh/uiprojects/threelinerotview/ThreeLineRotStepView.kt
+++ threelinerotview/src/main/java/com/anwesh/uiprojects/threelinerotview/ThreeLineRotStepView.kt
@@ -99,4 +99,32 @@ class ThreeLineRotStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start(cb : () -> Unit) {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
