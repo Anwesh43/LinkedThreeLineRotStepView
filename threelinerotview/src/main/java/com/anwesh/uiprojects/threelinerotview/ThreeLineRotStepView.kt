a9a8992b02c2502e20566db82b25c9a96cfce7d2
Anwesh43 <anweshthecool0@gmail.com>
2018-11-18 16:34:00 +0530

    adding ThreeLineRotStep class

--- threelinerotview/src/main/java/com/anwesh/uiprojects/threelinerotview/ThreeLineRotStepView.kt
+++ threelinerotview/src/main/java/com/anwesh/uiprojects/threelinerotview/ThreeLineRotStepView.kt
@@ -172,4 +172,30 @@ class ThreeLineRotStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ThreeLineRotStep(var i : Int, val state : State = State()) {
+
+        private val root : TLRSNode = TLRSNode(0)
+
+        private var curr : TLRSNode = root
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
