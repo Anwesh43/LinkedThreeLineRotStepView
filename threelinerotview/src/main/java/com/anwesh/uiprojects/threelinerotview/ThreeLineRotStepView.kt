2af14c5d3abfdf038ef91844b0d24e82974394ff
Anwesh43 <anweshthecool0@gmail.com>
2018-11-18 16:29:04 +0530

    adding TLRNode class

--- threelinerotview/src/main/java/com/anwesh/uiprojects/threelinerotview/ThreeLineRotStepView.kt
+++ threelinerotview/src/main/java/com/anwesh/uiprojects/threelinerotview/ThreeLineRotStepView.kt
@@ -127,4 +127,49 @@ class ThreeLineRotStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TLRSNode(var i : Int, val state : State = State()) {
+
+        private var prev : TLRSNode? = null
+
+        private var next : TLRSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = TLRSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTLRNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TLRSNode {
+            var curr : TLRSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
