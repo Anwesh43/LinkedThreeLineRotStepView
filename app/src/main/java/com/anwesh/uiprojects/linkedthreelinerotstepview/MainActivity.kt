b79a5d2b5870fe5ecd2d2420a22bb2c8f9398c94
Anwesh43 <anweshthecool0@gmail.com>
2018-11-18 16:50:00 +0530

    adding view in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedthreelinerotstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedthreelinerotstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedthreelinerotstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.threelinerotview.ThreeLineRotStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ThreeLineRotStepView.create(this)
     }
 }
