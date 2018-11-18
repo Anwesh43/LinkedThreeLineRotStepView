e83915625d6446d5ddadb55f8c6f3d2917f3e406
Anwesh43 <anweshthecool0@gmail.com>
2018-11-18 15:07:47 +0530

    adding libraries and defining nodes

--- threelinerotview/src/main/java/com/anwesh/uiprojects/threelinerotview/ThreeLineRotStepView.kt
+++ threelinerotview/src/main/java/com/anwesh/uiprojects/threelinerotview/ThreeLineRotStepView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.threelinerotview
 /**
  * Created by anweshmishra on 18/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 2
+val SIZE_FACTOR : Int = 3
+val STROKE_FACTOR : Int = 60
+val scDiv : Double = 0.5
+val scGap : Float = 0.05f
+val COLOR : Int = Color.parseColor("#0277BD")
+val BACK_COLOR : Int = Color.parseColor("#BDBDBD")
