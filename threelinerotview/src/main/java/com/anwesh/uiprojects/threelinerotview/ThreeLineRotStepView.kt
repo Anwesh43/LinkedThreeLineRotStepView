596b973cc36b5d00c6a0cefecf3e3002937149f3
Anwesh43 <anweshthecool0@gmail.com>
2018-11-18 15:16:13 +0530

    adding Float canvas extension method

--- threelinerotview/src/main/java/com/anwesh/uiprojects/threelinerotview/ThreeLineRotStepView.kt
+++ threelinerotview/src/main/java/com/anwesh/uiprojects/threelinerotview/ThreeLineRotStepView.kt
@@ -20,3 +20,15 @@ val scDiv : Double = 0.5
 val scGap : Float = 0.05f
 val COLOR : Int = Color.parseColor("#0277BD")
 val BACK_COLOR : Int = Color.parseColor("#BDBDBD")
+
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.max(0f, this - i * n.getInverse())) * n
+
+fun Float.getScaleFactor() : Float =  Math.floor(this / scDiv).toFloat()
+
+fun Float.getMirrorValue(a : Int, b : Int) : Float = (1 - this) * a.getInverse() + b.getInverse() * this
+
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = dir * scGap * getScaleFactor().getMirrorValue(a, b)
+
+
